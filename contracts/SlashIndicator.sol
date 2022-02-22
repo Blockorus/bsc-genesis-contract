5410ad16eba6bfe1b82f2dda1e4b194824eb8fca
Roshan <luoshen1997@gmail.com>
2022-02-22 10:52:58 +0800

    [WIP]Implement Fast Finality
    Ref: https://github.com/bnb-chain/BEPs/pull/126

--- contracts/SlashIndicator.sol
+++ contracts/SlashIndicator.sol
@@ -1,12 +1,14 @@
 pragma solidity 0.6.4;
 import "./System.sol";
 import "./lib/BytesToTypes.sol";
+import "./lib/TypesToBytes.sol";
 import "./lib/Memory.sol";
 import "./interface/ISlashIndicator.sol";
 import "./interface/IApplication.sol";
 import "./interface/IBSCValidatorSet.sol";
 import "./interface/IParamSubscriber.sol";
 import "./interface/ICrossChain.sol";
+import "./interface/ISystemReward.sol";
 import "./lib/CmnPkg.sol";
 import "./lib/RLPEncode.sol";
 
@@ -17,6 +19,8 @@ contract SlashIndicator is ISlashIndicator,System,IParamSubscriber, IApplication
   uint256 public constant FELONY_THRESHOLD = 150;
   uint256 public constant BSC_RELAYER_REWARD = 1e16;
   uint256 public constant DECREASE_RATE = 4;
+  uint256 public constant FINALITY_DISTANCE = 11;
+  uint256 public constant FINALITY_REWARD_RATIO = 20;
 
   // State of the contract
   address[] public validators;
@@ -24,6 +28,8 @@ contract SlashIndicator is ISlashIndicator,System,IParamSubscriber, IApplication
   uint256 public previousHeight;
   uint256 public  misdemeanorThreshold;
   uint256 public  felonyThreshold;
+  uint256 public finalityDistance;
+  uint256 public finalityRewardRatio;
 
   event validatorSlashed(address indexed validator);
   event indicatorCleaned();
@@ -39,6 +45,16 @@ contract SlashIndicator is ISlashIndicator,System,IParamSubscriber, IApplication
     bool exist;
   }
 
+  struct FinalityEvidence {
+    uint256 numA;
+    bytes32 headerA;
+    bytes   sigA;
+    uint256 numB;
+    bytes32 headerB;
+    bytes   sigB;
+    address valAddr;
+  }
+
   modifier oncePerBlock() {
     require(block.number > previousHeight, "can not slash twice in one block");
     _;
@@ -46,15 +62,22 @@ contract SlashIndicator is ISlashIndicator,System,IParamSubscriber, IApplication
   }
 
   modifier onlyZeroGasPrice() {
-    
     require(tx.gasprice == 0 , "gasprice is not zero");
-    
     _;
+  }
+
+  modifier noReentrant() {
+    require(!locked, "no re-entrancy");
+    locked = true;
+    _;
+    locked = false;
   }
   
   function init() external onlyNotInit{
     misdemeanorThreshold = MISDEMEANOR_THRESHOLD;
     felonyThreshold = FELONY_THRESHOLD;
+    finalityDistance = FINALITY_DISTANCE;
+    finalityRewardRatio = FINALITY_REWARD_RATIO;
     alreadyInit = true;
   }
 
@@ -158,6 +181,53 @@ contract SlashIndicator is ISlashIndicator,System,IParamSubscriber, IApplication
     emit indicatorCleaned();
   }
 
+  function handleFinalityReport(FinalityEvidence calldata _evidence) external onlyInit oncePerBlock noReentrant {
+    uint256 _numA = _evidence.numA;
+    uint256 _numB = _evidence.numB;
+
+
+    require(_numA + finalityDistance >= _numB || _numB + finalityDistance >= _numA, "too long distance between blocks");
+    require(
+      _numA <= block.number() && _numA + 256 >= block.number() && _numB <= block.number() && _numB + 256 >= block.number(),
+      "block number out of range"
+    );
+
+    uint256 index = IBSCValidatorSet(VALIDATOR_CONTRACT_ADDR).currentValidatorSetMap(_evidence.valAddr);
+    require(index > 0, "validator not exist")
+    ( , , , , , , bytes memory BLSKey) = IBSCValidatorSet(VALIDATOR_CONTRACT_ADDR).currentValidatorSet(index - 1)
+
+    bytes[7] memory input;
+
+    input[0] = TypesToBytes.uintToBytes(_evidence.numA);
+    input[1] = TypesToBytes.bytes32ToBytes(_evidence.headerA);
+    input[2] = TypesToBytes.uintToBytes(_evidence.numB);
+    input[3] = TypesToBytes.bytes32ToBytes(_evidence.headerB);
+    input[4] = _evidence.sigA;
+    input[5] = _evidence.sigB;
+    input[6] = BLSKey;
+
+    assembly {
+      if iszero(call(not(0), 0x13, 0, input, 0xe0, _, 0x20)) {
+        revert(0, 0)
+      }
+    }
+
+    bytes _headerA = blockhash(_numA);
+    bytes _headerB = blockhash(_numB);
+
+    if (_headerA != _evidence.headerA && _headerB != _evidence.headerB) {
+      revert(string(abi.encodePacked("invalid header")));
+    } else if (_headerA == _evidence.headerA && _headerB == _evidence.headerB) {
+      revert(string(abi.encodePacked("invalid evidence")));
+    } else {
+      uint256 amount = (address(SYSTEM_REWARD_ADDR).balance * finalityRewardRatio) / 100;
+      ISystemReward(SYSTEM_REWARD_ADDR).claimRewards(msg.sender, amount);
+      IBSCValidatorSet(VALIDATOR_CONTRACT_ADDR).felony(_evidence.valAddr);
+      ICrossChain(CROSS_CHAIN_CONTRACT_ADDR).sendSynPackage(SLASH_CHANNELID, encodeSlashPackage(_evidence.valAddr), 0);
+      emit validatorSlashed(_evidence.valAddr);
+    }
+  }
+
   function sendFelonyPackage(address validator) external override(ISlashIndicator) onlyValidatorContract onlyInit {
     ICrossChain(CROSS_CHAIN_CONTRACT_ADDR).sendSynPackage(SLASH_CHANNELID, encodeSlashPackage(validator), 0);
   }
@@ -174,6 +244,16 @@ contract SlashIndicator is ISlashIndicator,System,IParamSubscriber, IApplication
       uint256 newFelonyThreshold = BytesToTypes.bytesToUint256(32, value);
       require(newFelonyThreshold <= 1000 && newFelonyThreshold > misdemeanorThreshold, "the felonyThreshold out of range");
       felonyThreshold = newFelonyThreshold;
+    } else if (Memory.compareStrings(key, "finalityDistance")) {
+      require(value.length == 32, "length of finalityDistance mismatch");
+      uint256 newFinalityDistance = BytesToTypes.bytesToUint256(32, value);
+      require(newFinalityDistance >= 1 && newFinalityDistance <= 21, "the finality distance out of range");
+      finalityDistance = newFinalityDistance;
+    } else if (Memory.compareStrings(key, "finalityRewardRatio")) {
+      require(value.length == 32, "length of finalityRewardRatio mismatch");
+      uint256 newFinalityRewardRatio = BytesToTypes.bytesToUint256(32, value);
+      require(newFinalityRewardRatio >= 10 && newFinalityRewardRatio < 100, "the finality reward ratio out of range");
+      finalityRewardRatio = newFinalityRewardRatio;
     } else {
       require(false, "unknown param");
     }
