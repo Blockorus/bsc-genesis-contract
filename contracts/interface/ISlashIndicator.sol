735bf898cd5c5ab83f3bebad3624e995c6a94efa
Roshan <luoshen1997@gmail.com>
2022-03-24 18:04:36 +0800

    Update for slash test

--- contracts/interface/ISlashIndicator.sol
+++ contracts/interface/ISlashIndicator.sol
@@ -1,7 +1,17 @@
 pragma solidity 0.6.4;
 
 interface ISlashIndicator {
+  struct FinalityEvidence {
+    uint256 numA;
+    bytes32 headerA;
+    bytes sigA;
+    uint256 numB;
+    bytes32 headerB;
+    bytes sigB;
+    address valAddr;
+  }
   function clean() external;
   function sendFelonyPackage(address validator) external;
   function getSlashThresholds() external view returns (uint256, uint256);
+  function submitFinalityViolationEvidence(FinalityEvidence calldata _evidence) external onlyInit onlyRelayer;
 }
