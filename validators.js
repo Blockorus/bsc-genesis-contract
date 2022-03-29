895f35762650639b2874761bccd1f97dac736829
Roshan <48975233+Loverush@users.noreply.github.com>
2022-03-29 10:48:48 +0800

    Update for QA test (#13)
    * [WIP]Fast Finality: reward distribution and slash parts
    
    * Update codes
    
    * Update codes
    
    * Update codes
    
    * Update codes
    
    * Update codes
    
    * Fix review comments and update test
    
    * Fix code errors related contract SystemReward and BSCValidatorSet
    
    * Fix code errors related invalid opcode and add new api
    
    * Optimize gasused of updateValidator and update test
    
    * Optimize gasused of updateValidator and update test
    
    * Minor error fixed
    
    * Minor error fixed
    
    * Fix review comments and update scripts
    
    * Fix init issue
    
    * Fix external view issue and update test
    
    * Fix review comments
    
    * Fix review comments
    
    * Update for integration
    
    * Update for integration
    
    * Update for integration
    
    * Update for integration
    
    * Update for integration
    
    * Update for integration
    
    * Update for integration
    
    * Update contract abi files
    
    * Update contract abi files
    
    * Update contract abi files
    
    * Update for test
    
    * Update for test
    
    * Update for test
    
    * Update for test
    
    * Update for slash test
    
    * Update for slash test
    
    * Update for slash test
    
    * Update for slash test
    
    * Update for slash test
    
    * Update .gitignore
    
    * Add onlyRelayer modifier

--- validators.js
+++ validators.js
@@ -10,44 +10,50 @@ const validators = [
     votingPower: 0x0000000000000064
   }
 ];
+const bLSPublicKeys = [
+  "0xa3c03b91d566e1a790a54393818230b35c01102456a3c4d59995d110a9ee3c2035c2c1594b198e2172d4130970706318",
+];
 
 // ===============  Do not edit below ====
-function generateExtradata(validators) {
-  let extraVanity =Buffer.alloc(32);
-  let validatorsBytes = extraDataSerialize(validators);
-  let extraSeal =Buffer.alloc(65);
-  return Buffer.concat([extraVanity,validatorsBytes,extraSeal]);
+function generateExtradata(validators, bLSPublicKeys) {
+  let extraVanity = Buffer.alloc(32);
+  let validatorsBytes = extraDataSerialize(validators, bLSPublicKeys);
+  let extraSeal = Buffer.alloc(65);
+  return Buffer.concat([extraVanity, validatorsBytes, extraSeal]);
 }
 
-function extraDataSerialize(validators) {
+function extraDataSerialize(validators, bLSPublicKeys) {
   let n = validators.length;
   let arr = [];
-  for(let i = 0;i<n;i++){
+  for (let i = 0; i < n; i++) {
     let validator = validators[i];
+    let BLSPublicKey = bLSPublicKeys[i];
     arr.push(Buffer.from(web3.utils.hexToBytes(validator.consensusAddr)));
+    arr.push(Buffer.from(web3.utils.hexToBytes(BLSPublicKey)));
   }
   return Buffer.concat(arr);
 }
 
-function validatorUpdateRlpEncode(validators) {
+function validatorUpdateRlpEncode(validators, bLSPublicKeys) {
   let n = validators.length;
   let vals = [];
-  for(let i = 0;i<n;i++) {
+  for (let i = 0; i < n; i++) {
     vals.push([
       validators[i].consensusAddr,
       validators[i].bscFeeAddr,
       validators[i].feeAddr,
       validators[i].votingPower,
+      bLSPublicKeys[i]
     ]);
   }
   let pkg = [0x00, vals];
   return web3.utils.bytesToHex(RLP.encode(pkg));
 }
 
-extraValidatorBytes = generateExtradata(validators);
-validatorSetBytes = validatorUpdateRlpEncode(validators);
+extraValidatorBytes = generateExtradata(validators, bLSPublicKeys);
+validatorSetBytes = validatorUpdateRlpEncode(validators, bLSPublicKeys);
 
 exports = module.exports = {
   extraValidatorBytes: extraValidatorBytes,
   validatorSetBytes: validatorSetBytes,
-}
+};
