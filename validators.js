1e21a4a60540154da6d4614c813eb28aa3cf1bcb
Roshan <luoshen1997@gmail.com>
2022-04-01 16:06:19 +0800

    Update for new fast finality

--- validators.js
+++ validators.js
@@ -3,31 +3,15 @@ const RLP = require('rlp');
 
 // Configure
 const validators = [
-  
-   {
-     "consensusAddr": "0x14DE643980726167f9772623ce337940Bc2b0Aa0",
-     "feeAddr": "0x14DE643980726167f9772623ce337940Bc2b0Aa0",
-     "bscFeeAddr": "0x14DE643980726167f9772623ce337940Bc2b0Aa0",
-     "votingPower": 0x0000000010000000,
-   },
-   {
-     "consensusAddr": "0x3c2a0A9690A247c4DDc6f68EE5A41522996923DB",
-     "feeAddr": "0x3c2a0A9690A247c4DDc6f68EE5A41522996923DB",
-     "bscFeeAddr": "0x3c2a0A9690A247c4DDc6f68EE5A41522996923DB",
-     "votingPower": 0x0000000010000000,
-   },
-   {
-     "consensusAddr": "0x2b7C653375F4346336760A8066A6c871Aee70951",
-     "feeAddr": "0x2b7C653375F4346336760A8066A6c871Aee70951",
-     "bscFeeAddr": "0x2b7C653375F4346336760A8066A6c871Aee70951",
-     "votingPower": 0x0000000010000000,
-   },
+  {
+    consensusAddr: "0x9fB29AAc15b9A4B7F17c3385939b007540f4d791",
+    feeAddr: "0x9fB29AAc15b9A4B7F17c3385939b007540f4d791",
+    bscFeeAddr: "0x9fB29AAc15b9A4B7F17c3385939b007540f4d791",
+    votingPower: 0x0000000000000064
+  }
 ];
 const bLSPublicKeys = [
-  
    "0x85e6972fc98cd3c81d64d40e325acfed44365b97a7567a27939c14dbc7512ddcf54cb1284eb637cfa308ae4e00cb5588",
-   "0x8addebd6ef7609df215e006987040d0a643858f3a4d791beaa77177d67529160e645fac54f0d8acdcd5a088393cb6681",
-   "0x89abcc45efe76bec679ca35c27adbd66fb9712a278e3c8530ab25cfaf997765aee574f5c5745dbb873dbf7e961684347",
 ];
 
 // ===============  Do not edit below ====
